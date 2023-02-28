fa4c921f533b030b4ff3daece8e8eb441fa32deb
le-michael <michael.le647@gmail.com>
2023-02-27 20:22:12 -0800

    Set and get tutorial language from url

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,19 @@
-import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js";
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+function created() {
+  let url = new URL(window.location.href);
+  const lang = url.searchParams.get("lang");
+  if (lang) {
+    this.tutorialLanguage = lang;
+  }
+}
+
+function setTutorialLanguage(lang) {
+  let url = new URL(window.location.href);
+  url.searchParams.set("lang", lang);
+  window.history.pushState({}, "", url);
+  this.tutorialLanguage = lang;
+}
 
 createApp({
   data() {
@@ -6,5 +21,9 @@ createApp({
       tutorialLanguage: "C++",
     };
   },
+  methods: {
+    setTutorialLanguage: setTutorialLanguage,
+  },
+  created: created,
   delimiters: ["${", "}$"],
 }).mount("#app");
